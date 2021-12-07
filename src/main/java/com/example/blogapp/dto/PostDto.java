fb2b5c95df8bac8d4007d01653585691d7ff9333
Samson Aderibigbe <samsontimilehin@gmail.com>
2021-12-08 00:45:52 +0100

    update

--- src/main/java/com/example/blogapp/dto/PostDto.java
+++ src/main/java/com/example/blogapp/dto/PostDto.java
@@ -1,8 +1,10 @@
 package com.example.blogapp.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PostDto {
 
     private String title;
--- src/test/java/com/example/blogapp/repository/PostRepositoryTest.java
+++ src/test/java/com/example/blogapp/repository/PostRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.example.blogapp.repository;
 
+import com.example.blogapp.dto.PostDto;
 import com.example.blogapp.entity.Post;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,8 +18,21 @@ class PostRepositoryTest {
     private PostRepository postRepository;
 
     @Test
+    @Transactional
     void savePost(){
 
-        Post post = Post.builder().build();
+        PostDto postDto = PostDto.builder()
+                .title("Common mistakes")
+                .body("Discussing common errors").build();
+
+        Post post = Post.builder()
+                .title(postDto.getTitle())
+                .body(postDto.getBody()).build();
+        postRepository.save(post);
+
+        String postTitle = "Common mistakes";
+
+        Post expected = postRepository.findPostByTitle(post.getTitle());
+        assertEquals(expected, postRepository.findPostByTitle(postTitle));
     }
 }
